f12a256e3b0258d2cb68dcc7f5bafced6ed5325e
Anna Goncharova <147106427+Goanni@users.noreply.github.com>
2024-04-28 22:54:49 +0300

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,54 +1,53 @@
 // Copyright 2024 Goncharova Anna
-#include "pch.h"
 #include <string>
-#include "C:/Users/Anna Goncharova/Desktop/мипс1/laba6/textgen.cpp"
+#include "textgen.cpp"
+#include "pch.h"
 
 TEST(test1, Prefix) {
-	Generator generator;
-	std::string text = "One day Lily had a dream.";
-	int prefixLength = 2;
-	generator.creating_statetab(text, prefixLength);
-	Generator::prefix _prefix = { "One", "day" };
-	EXPECT_NE(generator.statetab.find(_prefix), generator.statetab.end());
+   Generator generator;
+   std::string text = "One day Lily had a dream.";
+   int prefixLength = 2;
+   generator.creating_statetab(text, prefixLength);
+   Generator::prefix _prefix = { "One", "day" };
+   EXPECT_NE(generator.statetab.find(_prefix), generator.statetab.end());
 }
 
 TEST(test2, Prefix_Suffix) {
-	Generator generator;
-	std::string text = "One day Lily had a dream.";
-	int prefixLength = 2;
-	generator.creating_statetab(text, prefixLength);
-	Generator::prefix _prefix = { "One", "day" };
-	EXPECT_EQ(generator.statetab[_prefix][0], "Lily");
+    Generator generator;
+    std::string text = "One day Lily had a dream.";
+    int prefixLength = 2;
+    generator.creating_statetab(text, prefixLength);
+    Generator::prefix _prefix = { "One", "day" };
+    EXPECT_EQ(generator.statetab[_prefix][0], "Lily");
 }
 TEST(test3, OnceSuffix) {
-	Generator generator;
-
-	std::string text = "One day Lily had a dream.";
-	int prefixLength = 2;
-	generator.creating_statetab(text, prefixLength);
-	std::string result = generator.Text_generator(1, 3);
-	EXPECT_EQ(result, "One day Lily ");
+    Generator generator;
+    std::string text = "One day Lily had a dream.";
+    int prefixLength = 2;
+    generator.creating_statetab(text, prefixLength);
+    std::string result = generator.Text_generator(1, 3);
+    EXPECT_EQ(result, "One day Lily ");
 }
 TEST(test4, VectorSuffix) {
-	Generator generator;
-	std::string text = "one two three four five six seven eight nine ten eleven twelve";
-	int prefixLength = 2;
-	generator.creating_statetab(text, prefixLength);
-	std::string result = generator.Text_generator(2, 302);
-	EXPECT_EQ(result, "one two three four ");
+    Generator generator;
+    std::string text = "one two three four five six seven eight nine ten eleven twelve";
+    int prefixLength = 2;
+    generator.creating_statetab(text, prefixLength);
+    std::string result = generator.Text_generator(2, 302);
+    EXPECT_EQ(result, "one two three four ");
 }
 
 TEST(test5, Table) {
-	Generator generator;
-	std::string text = "one two six 1 one two six 2 one two six 3";
-	int prefixLength = 2;
-	std::map < Generator::prefix, std::vector < std::string >> Statetab = {
-	{{"one", "two"}, {"six"}},
-	{{"two", "six"}, {"1", "2","3"}},
-	{{"six", "1"}, {"one"}},
-	{{"1", "one"}, {"two"}}
-	};
-	generator.trainFromStateTable(Statetab);
-	std::string result = generator.Text_generator(2, 302);
-	EXPECT_EQ(result, "1 one two six ");
+    Generator generator;
+    std::string text = "one two six 1 one two six 2 one two six 3";
+    int prefixLength = 2;
+    std::map < Generator::prefix, std::vector < std::string >> Statetab = {
+    {{"one", "two"}, {"six"}},
+    {{"two", "six"}, {"1", "2","3"}},
+    {{"six", "1"}, {"one"}},
+    {{"1", "one"}, {"two"}}
+    };
+    generator.trainFromStateTable(Statetab);
+    std::string result = generator.Text_generator(2, 302);
+    EXPECT_EQ(result, "1 one two six ");
 }
