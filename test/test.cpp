7d94ea4e19e64ce50a0803204f86617fa2c72d45
Anna Goncharova <147106427+Goanni@users.noreply.github.com>
2024-04-28 23:13:37 +0300

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,15 +1,15 @@
 // Copyright 2024 Goncharova Anna
 #include <string>
-#include "textgen.cpp"
+#include "../include/textgen.h"
 #include "pch.h"
 
 TEST(test1, Prefix) {
-   Generator generator;
-   std::string text = "One day Lily had a dream.";
-   int prefixLength = 2;
-   generator.creating_statetab(text, prefixLength);
-   Generator::prefix _prefix = { "One", "day" };
-   EXPECT_NE(generator.statetab.find(_prefix), generator.statetab.end());
+    Generator generator;
+    std::string text = "One day Lily had a dream.";
+    int prefixLength = 2;
+    generator.creating_statetab(text, prefixLength);
+    Generator::prefix _prefix = { "One", "day" };
+    EXPECT_NE(generator.statetab.find(_prefix), generator.statetab.end());
 }
 
 TEST(test2, Prefix_Suffix) {
@@ -30,7 +30,8 @@ TEST(test3, OnceSuffix) {
 }
 TEST(test4, VectorSuffix) {
     Generator generator;
-    std::string text = "one two three four five six seven eight nine ten eleven twelve";
+    std::string text = 
+    "one two three four five six seven eight nine ten eleven twelve";
     int prefixLength = 2;
     generator.creating_statetab(text, prefixLength);
     std::string result = generator.Text_generator(2, 302);
@@ -43,7 +44,7 @@ TEST(test5, Table) {
     int prefixLength = 2;
     std::map < Generator::prefix, std::vector < std::string >> Statetab = {
     {{"one", "two"}, {"six"}},
-    {{"two", "six"}, {"1", "2","3"}},
+    {{"two", "six"}, {"1", "2","3"}}, 
     {{"six", "1"}, {"one"}},
     {{"1", "one"}, {"two"}}
     };
